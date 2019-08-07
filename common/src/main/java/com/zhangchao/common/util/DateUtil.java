51857d49a35c8bf43acedb97cc292a428209b076
张超 <zhangchao042@ke.com>
2019-08-07 18:40:47 +0800

    log和date工具类创建封装

---
+++ common/src/main/java/com/zhangchao/common/util/DateUtil.java
@@ -0,0 +1,38 @@
+package com.zhangchao.common.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 创建时间: 2019/08/07
+ * 作者: zhangchao042@ke.com
+ * 描述: 日期操作相关的工具类
+ */
+public class DateUtil {
+
+  private static final String[] DayOfWeek = new String[]{
+      "星期一","星期二","星期三","星期四","星期五","星期六","星期日"
+  };
+
+  private DateUtil(){ }
+
+  /**
+   * 获取当前日期时间
+   * @param format 格式
+   * @return String类型日期时间
+   */
+  public static String getCurrentDate(String format){
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+    Date date = new Date(System.currentTimeMillis());
+    return simpleDateFormat.format(date);
+  }
+
+  /**
+   * 获取当前星期几
+   */
+  public static String getDayOfWeek(){
+    return DayOfWeek[Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 2];
+  }
+
+}
---
+++ common/src/main/java/com/zhangchao/common/util/LogUtil.java
@@ -0,0 +1,35 @@
+package com.zhangchao.common.util;
+
+import android.util.Log;
+
+/**
+ * 创建时间: 2019/08/07
+ * 作者: zhangchao042@ke.com
+ * 描述:Log日志工具类
+ */
+public class LogUtil {
+
+  //默认使用的tag
+  public static final String TAG = "ztc";
+
+  private LogUtil(){
+
+  }
+
+  public static void i(String msg){
+    Log.i(TAG,msg);
+  }
+
+  public static void d(String msg){
+    Log.d(TAG,msg);
+  }
+
+  public static void w(String msg){
+    Log.w(TAG,msg);
+  }
+
+  public static void e(String msg){
+    Log.e(TAG,msg);
+  }
+
+}
