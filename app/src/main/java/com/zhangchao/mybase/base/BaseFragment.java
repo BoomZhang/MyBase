98521507a8ca06662ad87813413e5cb7ef1668c7
qiudengjiao <choudengjiao001@ke.com>
2019-10-23 11:11:11 +0800

    <add>activity和fragment基类

---
+++ app/src/main/java/com/zhangchao/mybase/base/BaseFragment.java
@@ -0,0 +1,11 @@
+package com.zhangchao.mybase.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * 创建时间: 2019/10/23 11:09 <br>
+ * 作者: qiudengjiao <br>
+ * 描述: Fragment 基类
+ */
+public class BaseFragment extends Fragment {
+}
