36d7228cb8618c2797069936e17e7f22541098c8
qiudengjiao <choudengjiao001@ke.com>
2019-10-23 11:11:11 +0800

    <add>activity和fragment基类

---
+++ app/src/main/java/com/zhangchao/mybase/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.zhangchao.mybase.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 创建时间: 2019/10/23 11:08 <br>
+ * 作者: qiudengjiao <br>
+ * 描述: Activity 基类
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
